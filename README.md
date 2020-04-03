f46d56810cd2991edcd2f4cfdbb835ac0d3adbd8
kostyaBro <indianor201203@gmail.com>
2020-04-04 02:10:33 +0500

    implement vk youtube and google

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 #AntiCovid19Extension
+
 This is an extension for remove all information about covid-19 from our information life.
+
+[Orignal icon](https://www.flaticon.com/free-icon/stop_2750891)
---
+++ content.js
@@ -0,0 +1,78 @@
+console.log("AntiCovidExtention was run.");
+
+let covidWords = [
+    "covid", "Covid", "COVID",
+    "coronavirus", "Coronavirus", "CORONAVIRUS",
+    "коронавирус", "Коронавирус", "КОРОНАВИРУС",
+    "коровавирус",
+    "Карантин", "карантин", "КАРАНТИН"];
+
+function covidCleaner() {
+    if (window.location.href.includes("youtube")) {
+        var videoCells = document.querySelectorAll(
+            ".ytd-rich-grid-video-renderer, " +
+            ".ytd-rich-item-renderer,  " +
+            ".ytd-video-renderer, " +
+            ".ytd-playlist-video-list-renderer"
+        );
+        for (var i = 0; i < videoCells.length; i++) {
+            for (var j = 0; j < covidWords.length; j++) {
+                if (videoCells[i].textContent.includes(covidWords[j])) {
+                    videoCells[i].hidden = true;
+                    break;
+                }
+            }
+        }
+    }
+
+    if (window.location.href.includes("google")) {
+        var foundedPageCells = document.querySelectorAll(
+            ".rc, " +
+            ".So9e7d" // hidden not work
+        );
+        for (var i = 0; i < foundedPageCells.length; i++) {
+            for (var j = 0; j < covidWords.length; j++) {
+                if (foundedPageCells[i].textContent.includes(covidWords[j])) {
+                    foundedPageCells[i].hidden = true;
+                    break;
+                }
+            }
+        }
+    }
+
+    if (window.location.href.includes("vk.com")) {
+        var foundedPageCells = document.querySelectorAll(
+            "._post_content"
+        );
+        for (var i = 0; i < foundedPageCells.length; i++) {
+            for (var j = 0; j < covidWords.length; j++) {
+                if (foundedPageCells[i].textContent.includes(covidWords[j])) {
+                    foundedPageCells[i].hidden = true;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+setInterval(covidCleaner(), 1000);
+
+// IF I use mutationObserver i lock page and even can't close it.
+
+// var mutationObserver = new MutationObserver(function(mutations) {
+//     // mutations.forEach(function(mutation) {
+//         covidCleaner();
+//     // });
+// });
+//
+// mutationObserver.observe(
+//     document.documentElement,
+//     {
+//         childList: true,
+//         attributes: true,
+//         subtree: true,
+//         characterData: true,
+//         attributeOldValue: true,
+//         characterDataOldValue: true
+//     }
+// );
